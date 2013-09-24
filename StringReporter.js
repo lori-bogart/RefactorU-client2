9c4c3b19dda6371b5c4b6b63999baa26a1a224ba
RU-002 <ru-002@Admins-Mac-mini-64.local>
2013-09-24 12:36:05 -0600

    correccted word.toLowerCase(word) to word.toLowerCase() , etc. eg I took out the contents between ()

--- StringReporter.js
+++ StringReporter.js
@@ -2,7 +2,7 @@ var word = prompt ("Enter any word:" );
 alert("You entered: " + word + 
 	"\nIt has this number of chars: " + word.length + 
 	"\nThe third char is: " + word.charAt(2) + 
-	"\nThe word in lowercase: " + word.toLowerCase(word) +
-	"\nThe word in UPPERCASE: " + word.toUpperCase(word) +
+	"\nThe word in lowercase: " + word.toLowerCase() +
+	"\nThe word in UPPERCASE: " + word.toUpperCase() +
 	"\nExample: I have been thinking of a " + word + " since yesterday." +
 	"\nSubword: " + word.substring(1,4))
